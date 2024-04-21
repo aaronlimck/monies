33fc995bf31bf29071e6ad2f829a648e1263fa7e
aaron <aaronlimchengkiat@gmail.com>
2024-04-21 22:27:05 +0800

    Update AvatarMenu component with userProps and SignOutBtn

--- components/navigation/AvatarMenu.tsx
+++ components/navigation/AvatarMenu.tsx
@@ -2,26 +2,47 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Session } from "next-auth";
+import Link from "next/link";
+import SignOutBtn from "../common/SignOutBtn";
 
 export default function AvatarMenu({
+  userProps,
   children,
 }: {
+  userProps: Session | null;
   children: React.ReactNode;
 }) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
-      <DropdownMenuContent align="end" alignOffset={2}>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+      <DropdownMenuContent
+        className="w-full max-w-[200px]"
+        align="end"
+        alignOffset={2}
+      >
+        <div className="px-2 py-1.5 text-sm font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">
+              {userProps?.user?.name}
+            </p>
+            <p className="text-xs leading-none text-neutral-400">
+              {userProps?.user?.email}
+            </p>
+          </div>
+        </div>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-        <DropdownMenuItem>Log Out</DropdownMenuItem>
+        <DropdownMenuItem>
+          <Link href="/overview" className="w-full">
+            Dashboard
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <SignOutBtn className="w-full cursor-pointer" label={"Log Out"} />
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
