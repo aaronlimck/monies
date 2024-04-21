7fde9f8c5286443ee0885b61d5f66c7c167f78b5
aaron <aaronlimchengkiat@gmail.com>
2024-04-21 22:28:38 +0800

    Update expense categories in categories.ts

--- lib/categories.ts
+++ lib/categories.ts
@@ -8,31 +8,30 @@ export const INCOME_CATEGORIES: { [key: string]: string } = {
   FREELANCE: "Freelance/Contract Income",
   GIFT: "Gift/Inheritance",
   OTHER: "Other Income",
-};
-
-export const EXPENSE_CATEGORIES: { [key: string]: string } = {
-  HOUSING: "Housing",
-  UTILITIES: "Utilities",
-  TRANSPORTATION: "Transportation",
-  FOOD: "Food/Groceries",
-  HEALTHCARE: "Healthcare",
-  EDUCATION: "Education",
-  ENTERTAINMENT: "Entertainment",
-  INSURANCE: "Insurance",
-  DEBT: "Debt Repayment",
-  CLOTHING: "Clothing",
-  SAVINGS: "Savings",
-  INVESTMENTS: "Investments",
-  CHARITY: "Charitable Donations",
-  TRAVEL: "Travel",
-  CHILDCARE: "Childcare",
-  PERSONAL_CARE: "Personal Care",
+  CASHBACK: "Cashback",
+  CASH_DEPOSIT: "Cash Deposit",
 };
 
 export const INCOME_CATEGORIES_KEYS = Object.keys(INCOME_CATEGORIES) as [
   string,
   ...string[],
 ];
+
+export const EXPENSE_CATEGORIES: { [key: string]: string } = {
+  ENTERTAINMENT: "Entertainment",
+  FOOD: "Food",
+  GIFT: "Gift",
+  HOLIDAY: "Holiday",
+  INVESTMENT: "Investment",
+  OTHERS: "Others",
+  PERSONAL_CARE: "Personal Care",
+  SAVING: "Saving",
+  SHOPPING: "Shopping",
+  TRANSPORT: "Transport",
+  UNCATEGORIZED: "Uncategorized",
+  UTILITIES: "Utilities",
+};
+
 export const EXPENSE_CATEGORIES_KEYS = Object.keys(EXPENSE_CATEGORIES) as [
   string,
   ...string[],
