49d22db2de0f7763860885f8d5cc4e89d2df1873
aaron <aaronlimchengkiat@gmail.com>
2024-04-22 00:06:24 +0800

    Update login page content and styles

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -9,11 +9,16 @@ export default function LoginPage({
     <div className="container flex h-dvh items-center justify-center">
       <div className="w-full max-w-sm">
         <h1 className="pb-4 text-3xl font-medium">Login to moolah.</h1>
-        <p className="pb-1 text-2xl font-medium text-[#878787]">
+        {/* <p className="pb-1 text-2xl font-medium text-[#878787]">
           Automate financial tasks, <br /> stay organized, and make
           <br />
           informed decisions
           <br /> effortlessly.
+        </p> */}
+        <p className="pb-1 text-2xl font-medium text-[#878787]">
+          Tools to manage your
+          <br /> finance with ease. <br />
+          Simple, fast, and secure.
         </p>
         <div className="my-6">
           <ContinueWithSocials callbackUrl="" />
